657624ae50dee63fc9eea9cf6f3f57d440a32ab7
Flealand <chrisgrass321@gmail.com>
2021-12-08 22:52:11 +0100

    Create install-programs.bat
    adds first install skript

---
+++ install-programs.bat
@@ -0,0 +1,11 @@
+@echo off
+winget install GIMP.GIMP
+winget install Discord.Discord
+winget install Battle.net
+winget install KeePassPasswordSafe2_is1
+winget install Origin
+winget install Notepad++.Notepad++
+winget install Valve.Steam
+winget install Microsoft.VisualStudioCode
+winget install Google.Chrome
+pause
