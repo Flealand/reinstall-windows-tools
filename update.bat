61e5e5f333197c9add24b04ca39453d57d29d936
Flealand <chrisgrass321@gmail.com>
2021-12-09 16:59:04 +0100

    adds update script

---
+++ update.bat
@@ -0,0 +1,18 @@
+@echo off
+echo updates gimp
+winget upgrade GIMP.GIMP
+echo updates discord
+winget upgrade Discord.Discord
+echo updates KeePass
+winget upgrade KeePassPasswordSafe2_is1
+echo updates Origin
+winget upgrade Origin
+echo updates Notepad++
+winget upgrade Notepad++.Notepad++
+echo updates Steam
+winget upgrade Valve.Steam
+echo updates VsCode
+winget upgrade Microsoft.VisualStudioCode
+echo updates Chrome
+winget upgrade Google.Chrome
+pause
