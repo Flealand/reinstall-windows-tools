0842744ae2f5488762eb88e99f3050a4661522e9
Flealand <chrisgrass321@gmail.com>
2025-09-19 11:26:06 +0200

    Update install-programs.bat

--- install-programs.bat
+++ install-programs.bat
@@ -1,11 +1,10 @@
 @echo off
-winget install GIMP.GIMP
 winget install Discord.Discord
-winget install Battle.net
-winget install KeePassPasswordSafe2_is1
-winget install Origin
+winget install eaapp
 winget install Notepad++.Notepad++
 winget install Valve.Steam
+winget install Mozilla.Firefox 
+winget install GIMP.GIMP
+winget install Battle.net
 winget install Microsoft.VisualStudioCode
-winget install Google.Chrome
 pause
